cf864aab526696917ff2e50b33b9a9591304fc44
tusharkhatri <109337129+tusharkhatri434@users.noreply.github.com>
2023-09-14 08:28:06 +0530

    Add files via upload

---
+++ Day-4 Access Camera by Javascript/main.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let but = document.getElementById("but");
+  let video = document.getElementById("vid");
+  let mediaDevices = navigator.mediaDevices;
+  vid.muted = true;
+  but.addEventListener("click", () => {
+    // Accessing the user camera and video.
+    mediaDevices
+      .getUserMedia({
+        video: true,
+        audio: true,
+      })
+      .then((stream) => {
+        // Changing the source of video to current stream.
+        video.srcObject = stream;
+        video.addEventListener("loadedmetadata", () => {
+          video.play();
+        });
+      })
+      .catch(alert);
+  });
+});
