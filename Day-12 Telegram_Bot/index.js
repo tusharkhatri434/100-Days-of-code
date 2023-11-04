cdd457b216d22e4b756d4859596998545c738260
tusharkhatri <109337129+tusharkhatri434@users.noreply.github.com>
2023-11-04 14:36:58 +0530

    Delete Day-12 Telegram_Bot directory

--- Day-12 Telegram_Bot/index.js
+++
@@ -1,7 +0,0 @@
-const {Telegraf} = require('telegraf');
-
-const bot = new Telegraf("6915274292:AAGnL-EFbwuKL_9jSblA3Je0d0UeN9k5hFw");
-
-bot.start((ctx)=> ctx.reply("welcome to tushar AI Bot"));
-bot.on("sticker", (ctx) => ctx.reply("👍"));
-bot.launch();
